5cb67f346c63a27a9f24fc970f9916b8dc77924b
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2019-11-21 14:48:13 +0700

    tin tức - quy định chung
    làm xong rồi hee

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,36 +1,36 @@
-var listBooks = [
-    {
+var listBooks = [{
         "name": "Agile Principles, Pattern and Practices in C#",
-        "author" : "Ahmedt-Aristizabal, David ; Fookes, Clinton",
-        "des" : '"The solution is a laptop computer. The main purpose of a computer is domination. A computer ensures you do not waste your time"',
-        "image" : '../assets/images/search/book1.png'
+        "author": "Ahmedt-Aristizabal, David ; Fookes, Clinton",
+        "des": '"The solution is a laptop computer. The main purpose of a computer is domination. A computer ensures you do not waste your time"',
+        "image": '../assets/images/search/book1.png'
     },
     {
         "name": "Introduction to Algorithms",
-        "author" : "Thomas",
-        "des" : '"The solution is a laptop computer. The main purpose of a computer is domination. A computer ensures you do not waste your time"',
-        "image" : '../assets/images/search/book2.png'
+        "author": "Thomas",
+        "des": '"The solution is a laptop computer. The main purpose of a computer is domination. A computer ensures you do not waste your time"',
+        "image": '../assets/images/search/book2.png'
     }
 ]
 
-function loadData(data){
+function loadData(data) {
 
-    data.forEach((book)=>{
+    data.forEach((
+        book) => {
         console.log(book);
         var bookInfo = $("<div class='book row'></div>");
         var col4 = $("<div class='col-4 book-img'></div>");
         var col8 = $("<div class='col-8 book-info'></div>");
-        col4.append("<img src="+book.image +">");
-        col8.append("<h1 class='book-title'>"+book.name +"</h1>");
-        col8.append("<h3 class='book-author'>"+book.author +"</h3>");
-        col8.append("<p class='book-des'>"+book.des +"</p>");
+        col4.append("<img src=" + book.image + ">");
+        col8.append("<h1 class='book-title'>" + book.name + "</h1>");
+        col8.append("<h3 class='book-author'>" + book.author + "</h3>");
+        col8.append("<p class='book-des'>" + book.des + "</p>");
         col8.append("<a class='borrowBookBtn' href='#'>Mượn sách</a>")
         bookInfo.append(col4);
         bookInfo.append(col8);
         $(".list-books").append(bookInfo);
     })
 
-    
+
 }
 
 
@@ -38,17 +38,15 @@ function loadData(data){
 window.onload = function() {
     loadData(listBooks);
 
-    $('#loginBtn').click(function(e){
+    $('#loginBtn').click(function(e) {
         var username = $('#username').val();
         var password = $("#password").val();
-        if (password === 'admin' && username ==='admin') {
+        if (password === 'admin' && username === 'admin') {
             alert("Đăng nhập thành công!!!");
             $('#formLogin').modal('hide');
-        }
-        else {
-            $(".noti-err").css("display","block");
+        } else {
+            $(".noti-err").css("display", "block");
             alert("Username :admin ; password : admin")
         }
     })
-  }
-
+}
